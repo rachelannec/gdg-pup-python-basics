0f2967ddf81584a203ef3dce1f1aa47154b75c45
Rachel Anne Cilon <annerachel095@gmail.com>
2025-02-24 09:27:59 +0800

    Add image file and example Python script for list operations

---
+++ Assignments-Exercises/Python-5_List-and-List-Operations/main-ans.py
@@ -0,0 +1,14 @@
+this_list = [10, 7, 3, 8, 9, 1, 2]
+print("Original list: ", this_list)
+
+# add an element to the list
+this_list.append(5)
+print("List after adding an element: ", this_list)
+
+# remove an element from the list
+this_list.remove(8)
+print("List after removing an element: ", this_list)
+
+# sort the list
+this_list.sort()
+print("Sorted list: ", this_list)
